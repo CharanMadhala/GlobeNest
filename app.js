03507fce1b4a159b1941fb6f135d7383a79e7214
Charan <charanmadala34@gmail.com>
2024-12-28 18:04:41 +0530

    Airbnb - [5] Index Route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const port = 8080;
 const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require("./models/listing.js");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 main().then(()=>{
     console.log("DB connection sucessful");
@@ -19,18 +23,25 @@ app.get("/", (req, res)=>{
     res.send("get request working");
 });
 
-app.get("/test/listing", async (req, res)=>{
-    let sampleListing = new Listing({
-        title: "My new Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India",
-    });
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("document created");
+//Index Route - to display all Listings
+app.get("/listings", async (req, res)=>{
+    const allListings = await Listing.find({});
+    res.render("\listings/index.ejs", {allListings});
 });
+
+
+// app.get("/test/listing", async (req, res)=>{
+//     let sampleListing = new Listing({
+//         title: "My new Villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India",
+//     });
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("document created");
+// });
 
 app.listen(8080, ()=>{
     console.log("listening to port 8080");
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
+<h2> Airbnb - Database(Wanderlust , cooolection: wanderlust)</h2>
+<ul>
+    <li>Index Route: GET /listings - to display all Listings</li>
+    <li>Show Route: GET /listings/:id - to display individual lsiting</li>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
 <!-- Phase 1 (part a)
 [1] basic setup -- installing express, ejs, mongoose. 
     index.js -- require express, initialize app, lister to port for requests, setup up home root "/" to verify working.
@@ -9,4 +17,6 @@
 initialise database, data link: https://github.com/apna-college/wanderlust/blob/main/init/data.js
 
 [4] Initilization of database: used to intialize databse with new data, helpfull whenever we need to reinitialise database. File: init/data.js (contains data).... File: init/index.js (contains required to code to cleanup existing database and insert intilisation data).
+
+[5] Index Route: GET /listings - to display all Listings
  -->
