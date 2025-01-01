6a0913f900179e10627e21168683a368276f7ba0
Charan <charanmadala34@gmail.com>
2025-01-01 22:56:13 +0530

    [14] 05. Styling New Listing (Add new listing page)

--- models/listing.js
+++ models/listing.js
@@ -20,8 +20,13 @@ const listingSchema = new Schema({
     //     },
     //     },
     image: {
-        filename: { type: String },
-        url: { type: String }
+        filename: { 
+            type: String 
+        },
+        url: { 
+            type: String,
+            default: "https://images.unsplash.com/photo-1484199383121-dfa3c30608cd?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
     },
     price: Number,
     location: String,
